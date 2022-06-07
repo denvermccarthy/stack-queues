4b8a9d220420d50b640aa57409eeaf96f2df1ef2
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-07 15:16:37 -0700

    implemented stack class

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-class Stack {}
+//push peek pop
 
+class Stack {
+  #list = [];
+  constructor(initialData) {
+    if (initialData) this.#list = initialData;
+  }
+  push(data) {
+    this.#list.push(data);
+  }
+  peek() {
+    return this.#list[this.#list.length - 1];
+  }
+  pop() {
+    return this.#list.pop();
+  }
+  view() {
+    return this.#list.toString();
+  }
+}
+
+const stack = new Stack([1, 2, 3]);
+stack.push(1);
+console.log(stack.peek());
+const popped = stack.pop();
+console.log("popped", popped);
+console.log("stack.view()", stack.view());
 class Queue {}
 
 module.exports = { Stack, Queue };
